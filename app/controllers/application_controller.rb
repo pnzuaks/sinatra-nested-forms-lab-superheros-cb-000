1fded5596f81fd7a61a382cef9c7bbba7e7589a7
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-13 18:00:11 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,24 @@
 require 'sinatra/base'
+require_relative '../models/team.rb'
+require_relative '../models/hero.rb'
+
 
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+
+      params[:team][:heroes].each do |details|
+        Hero.new(details)
+    end
+
+    erb :team
+  end
 
 end
